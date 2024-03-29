5320aced6cefcdad228d15f7b5946e682fddea46
Gwon Jincheol <145963704+Jincheol-11@users.noreply.github.com>
2024-03-30 03:28:47 +0900

    Update String_Date.sql

--- String_Date.sql
+++ String_Date.sql
@@ -24,3 +24,14 @@ FROM USED_GOODS_BOARD
 WHERE 1=1 // 참이라는 뜻 (CREATED_DATE = "2022-10-05%" 데이터를 모두 조회하라는 뜻)
 AND CREATED_DATE = "2022-10-05%"
 ORDER BY BOARD_ID DESC;
+
+
+USED_GOODS_BOARD와 USED_GOODS_FILE 테이블에서 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회하는 SQL문을 작성해주세요. 첨부파일 경로는 FILE ID를 기준으로 내림차순 정렬해주세요. 기본적인 파일경로는 /home/grep/src/ 이며, 
+게시글 ID를 기준으로 디렉토리가 구분되고, 파일이름은 파일 ID, 파일 이름, 파일 확장자로 구성되도록 출력해주세요. 조회수가 가장 높은 게시물은 하나만 존재합니다.
+
+SELECT CONCAT("/","home/grep/src","/",b.BOARD_ID,"/",FILE_ID,FILE_NAME,FILE_EXT)FILE_PATH
+FROM USED_GOODS_BOARD AS b, USED_GOODS_FILE AS f
+WHERE b.BOARD_ID = f.BOARD_ID
+    AND VIEWS = (SELECT MAX(VIEWS)
+             FROM USED_GOODS_BOARD)
+ORDER BY FILE_ID DESC;
